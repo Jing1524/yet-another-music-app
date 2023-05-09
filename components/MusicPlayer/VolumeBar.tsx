b8b6798a76e50f9b29a81366655d597b8da7f59f
jing <jingli1524@gmail.com>
2023-05-08 23:50:54 -0400

    type casing

--- components/MusicPlayer/VolumeBar.tsx
+++ components/MusicPlayer/VolumeBar.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BsFillVolumeUpFill, BsVolumeDownFill, BsFillVolumeMuteFill } from 'react-icons/bs';
+import React from 'react'
+import { BsFillVolumeUpFill, BsVolumeDownFill, BsFillVolumeMuteFill } from 'react-icons/bs'
 
-const VolumeBar = ({ value, min, max, onChange, setVolume }) => (
-  <div className="hidden lg:flex flex-1 items-center justify-end">
+const VolumeBar = ({ value, min, max, onChange, setVolume }: any) => (
+  <div className="items-center justify-end flex-1 hidden lg:flex">
     {value <= 1 && value > 0.5 && <BsFillVolumeUpFill size={25} color="#FFF" onClick={() => setVolume(0)} />}
     {value <= 0.5 && value > 0 && <BsVolumeDownFill size={25} color="#FFF" onClick={() => setVolume(0)} />}
     {value === 0 && <BsFillVolumeMuteFill size={25} color="#FFF" onClick={() => setVolume(1)} />}
@@ -13,9 +13,9 @@ const VolumeBar = ({ value, min, max, onChange, setVolume }) => (
       min={min}
       max={max}
       onChange={onChange}
-      className="2xl:w-40 lg:w-32 md:w-32 h-1 ml-2"
+      className="h-1 ml-2 2xl:w-40 lg:w-32 md:w-32"
     />
   </div>
-);
+)
 
-export default VolumeBar;
+export default VolumeBar
